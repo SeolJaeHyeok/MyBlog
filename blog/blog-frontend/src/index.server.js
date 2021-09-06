ec214a503ce0f314bf68cdb5ad5b2bfd08922397
Jaehyeok <milkboy2564@naver.com>
2021-09-07 02:55:57 +0900

    Set axios client baseURL
    1) 서버 엔트리 코드에서 axios 클라이언트 인스턴스 baseURL 설정

--- blog/blog-frontend/src/index.server.js
+++ blog/blog-frontend/src/index.server.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import client from './lib/api/client';
+
+client.defaults.baseURL = 'http://localhost:4000';
 
 const html = ReactDOMServer.renderToString(
   <div>Hello Server Side Rendering</div>,
